e80e5d938a43fea8b82684a430b0f431e4f58d16
Amani-pixel <83713668+Amani-pixel@users.noreply.github.com>
2021-06-16 16:29:24 +0200

    Add files via upload

---
+++ somme.algo.js
@@ -0,0 +1,17 @@
+ALGORITHME SUM 
+Var
+T1,T2:ARRAY_OF INTEGER[5];
+i,j:integer;
+S:integer:=0;
+BEGIN 
+for i form 0 to 5 do
+Read T1[i]
+for j from 0 to 5 do
+read    T2[j]
+if T1[i]<>T2[J] then 
+s:=s+T1[i]
+END if
+END For
+END FOR 
+END
+
